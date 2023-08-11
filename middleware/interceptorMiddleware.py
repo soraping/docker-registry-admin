48ee3e8ede4d45f4920ec5dcafd6b1923418def7
soraping <541251250@qq.com>
2023-08-11 16:47:50 +0800

    list

--- middleware/interceptorMiddleware.py
+++ middleware/interceptorMiddleware.py
@@ -1,4 +1,7 @@
 import logging
+import ujson
+from django.http import HttpRequest
+from utils import R, ErrorEnum
 
 from django.utils.deprecation import MiddlewareMixin
 
@@ -11,9 +14,14 @@ class InterceptorMiddleware(MiddlewareMixin):
         super().__init__(get_response)
         self.get_response = get_response
 
-
-    def process_request(self, request):
-        print(request)
+    def process_request(self, request: HttpRequest):
+        if request.method == 'POST':
+            try:
+                json_data = request.body.decode('utf-8')
+                dict_data = ujson.loads(json_data)
+                request.POST = dict_data
+            except Exception as e:
+                return R.failed(ErrorEnum.PARAMS_IS_ERROR)
 
     def process_response(self, request, response):
         result = response.content.decode('utf-8')
